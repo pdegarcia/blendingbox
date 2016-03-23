fd652e2dfc27832f82437221bafae934066ba073
Paulo Garcia <paulogarcia@PGiMac.local>
2016-03-23 04:16:35 +0000

    no message

--- Studies/js/thankyou.js
+++ Studies/js/thankyou.js
@@ -15,7 +15,7 @@ function submitForm() {
 
   $.ajax({
     type: "POST",
-    url: "php/contactForm-process.php",
+    url: "../php/form-process.php",
     data: "name=" + name + "&email=" + email + "&message=" + message,
     success: function(text) {
       if (text === "success") {
