fea5dae16b362f35e12c416a08024b9a1dd047d6
Paulo Garcia <paulo.garcia@tecnico.ulisboa.pt>
2016-03-28 18:39:54 +0100

    EN profiling without values. Standard between PT and EN.

--- Studies/js/profiling.js
+++ Studies/js/profiling.js
@@ -3,22 +3,53 @@
       submitForm();
 });*/
 
-/*var $academic = $('#academic');
-
-$.getJSON("../data/populate.json", function(data) {
-
-  console.log("oi");
-
-  var key = "grau";
-  var vals = data.grau.split(",");
-
-  $academic.html('');
-
-  console.log(vals);
-
-  $.each(vals, function(key, val) {
-    console.log(key + "," + val);
-    $academic.append("<option>" + val.name + "</option>");
-  });
-
+/*$('#profilingForm').formValidation({
+  framework: 'bootstrap',
+  icon: {
+    valid: 'glyphicon glyphicon-ok',
+    invalid: 'glyphicon glyphicon-remove',
+    validating: 'glyphicon glyphicon-refresh'
+  },
+  fields: {
+    inputAge: {
+      row: '.col-xs-4',
+      validators: {
+          notEmpty: {
+              message: 'Your Age is required'
+          }
+      }
+    },
+    gridRadios: {
+      row: '.col-xs-4',
+      validators: {
+          notEmpty: {
+              message: 'Your Gender is required'
+          }
+      }
+    },
+    academic: {
+      row: '.col-xs-4',
+      validators: {
+          notEmpty: {
+              message: 'Your Academic Degree is required'
+          }
+      }
+    },
+    nacional: {
+      row: '.col-xs-4',
+      validators: {
+          notEmpty: {
+              message: 'Your Nationality is required'
+          }
+      }
+    },
+    countryResidence: {
+      row: '.col-xs-4',
+      validators: {
+          notEmpty: {
+              message: 'Your Current Country of Residence is required'
+          }
+      }
+    },
+  }
 });*/
