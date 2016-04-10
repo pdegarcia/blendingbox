481f71293dacb5584823634c9b9eb3a040782457
Paulo Garcia <paulo.garcia@tecnico.ulisboa.pt>
2016-04-10 01:15:55 +0100

    INDEX principal: 🇬🇧 / INDEX secundário: 🇵🇹. Infos meta adicionadas para FB.

--- Studies/js/handleAlerts.js
+++ Studies/js/handleAlerts.js
@@ -3,13 +3,13 @@ function alertTitle() {
     case 'en':
       var r = confirm("Returning to homepage will cause you the lost of all information. Are you sure you want to proceed?");
       if (r === true) {
-        location.href = "../../indexEN.html";
+        location.href = "../../index.html";
       }
       break;
     case 'pt':
       var rpt = confirm("Voltar atrás fará com que perca toda a informação. Tem a certeza?");
       if (rpt === true) {
-        location.href = "../index.html";
+        location.href = "../indexPT.html";
       }
       break;
   }
