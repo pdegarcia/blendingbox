e4f9821dd09a439d286a9ea0e28579d4845692aa
Paulo Garcia <paulo.garcia@tecnico.ulisboa.pt>
2016-08-28 02:19:29 +0100

    🏅 All results were generated, for all 32 Questions. NEXT: Statistical Analysis from SPSS.

--- Analysis/First Study/Scripts/FirstStudyAnalysis_complete.m
+++ Analysis/First Study/Scripts/FirstStudyAnalysis_complete.m
@@ -24,48 +24,238 @@
 clear all;
 close all;
 
-% Convert values according to a .icc profile or not, generating RGB values
+%% Convert values according to a .icc profile or not, generating RGB values
 % Tables and Hexadecimal color codes.
+disp(' ');
+disp('-----------------------------------------------------------------------------------------');
+disp('|                                  COLOR CONVERTER                                      |');
+disp('-----------------------------------------------------------------------------------------');
+
 ColorConverter; 
 
-% Create Color Bins table and other stuff.
+disp('>> Color Converter runned smooth. Finished.');
+
+%% Create Color Bins table and other stuff.
+disp(' ');
+disp('-----------------------------------------------------------------------------------------');
+disp('|                                    COLOR BINS                                         |');
+disp('-----------------------------------------------------------------------------------------');
+
 process_colorBins; % -- UNCOMMENT HERE TO PROCESS ALL COLOR BINS (WARNING: extensively resource-consuming task).
+answers = ones(1,32);
 
-% Processing questions of type 'objTwoColors': OBJ (Given) = C1 (Requested) + C2 (Requested)
-q1_firstStudy_analysis;
-q2_firstStudy_analysis;
-q3_firstStudy_analysis;
-q4_firstStudy_analysis;
-q5_firstStudy_analysis;
-q6_firstStudy_analysis;
-q7_firstStudy_analysis;
-q8_firstStudy_analysis;
-q9_firstStudy_analysis;
-q10_firstStudy_analysis;
-q11_firstStudy_analysis;
-q12_firstStudy_analysis;
-q13_firstStudy_analysis;
-q14_firstStudy_analysis;
-q15_firstStudy_analysis;
-q16_firstStudy_analysis;
-q17_firstStudy_analysis;
+%% Processing questions of type 'objTwoColors': OBJ (Given) = C1 (Requested) + C2 (Requested)
+disp(' ');
+disp('-----------------------------------------------------------------------------------------');
+disp('|                                QUESTION ANALYSIS                                      |');
+disp('-----------------------------------------------------------------------------------------');
 
-% Processing questions of type 'twoColorsObj': C1 (Given) + C2 (Given) = OBJ (Requested)
-% q18_firstStudy_analysis;
-% q19_firstStudy_analysis;
-% q20_firstStudy_analysis;
-% q21_firstStudy_analysis;
-% q22_firstStudy_analysis;
-% q23_firstStudy_analysis;
-% q24_firstStudy_analysis;
-% q25_firstStudy_analysis;
-% q26_firstStudy_analysis;
-% q27_firstStudy_analysis;
-% q28_firstStudy_analysis;
-% q29_firstStudy_analysis;
-% q30_firstStudy_analysis;
-% q31_firstStudy_analysis;
-% q32_firstStudy_analysis;
+try 
+    q1_firstStudy_analysis;
+catch
+    answers(1,1) = 0;
+end
 
-% Close all plots generated.
+try 
+    q2_firstStudy_analysis;
+catch
+    answers(1,2) = 0;
+end
+
+try 
+    q3_firstStudy_analysis;
+catch
+    answers(1,3) = 0;
+end
+
+try 
+    q4_firstStudy_analysis;
+catch
+    answers(1,4) = 0;
+end
+
+try 
+    q5_firstStudy_analysis;
+catch
+    answers(1,6) = 0;
+end
+
+try 
+    q7_firstStudy_analysis;
+catch
+    answers(1,7) = 0;
+end
+
+try 
+    q8_firstStudy_analysis;
+catch
+    answers(1,8) = 0;
+end
+
+try 
+    q9_firstStudy_analysis;
+catch
+    answers(1,9) = 0;
+end
+
+try 
+    q10_firstStudy_analysis;
+catch
+    answers(1,10) = 0;
+end
+
+try 
+    q11_firstStudy_analysis;
+catch
+    answers(1,11) = 0;
+end
+
+try 
+    q12_firstStudy_analysis;
+catch
+    answers(1,12) = 0;
+end
+
+try 
+    q13_firstStudy_analysis;
+catch
+    answers(1,13) = 0;
+end
+
+try 
+    q14_firstStudy_analysis;
+catch
+    answers(1,14) = 0;
+end
+
+try 
+    q15_firstStudy_analysis;
+catch
+    answers(1,15) = 0;
+end
+
+try 
+    q16_firstStudy_analysis;
+catch
+    answers(1,16) = 0;
+end
+
+try 
+    q17_firstStudy_analysis;
+catch
+    answers(1,17) = 0;
+end
+
+try 
+    q18_firstStudy_analysis;
+catch
+    answers(1,18) = 0;
+end
+
+try 
+    q19_firstStudy_analysis;
+catch
+    answers(1,19) = 0;
+end
+
+try 
+    q20_firstStudy_analysis;
+catch
+    answers(1,20) = 0;
+end
+
+try 
+    q21_firstStudy_analysis;
+catch
+    answers(1,21) = 0;
+end
+
+try 
+    q22_firstStudy_analysis;
+catch
+    answers(1,22) = 0;
+end
+
+try 
+    q23_firstStudy_analysis;
+catch
+    answers(1,23) = 0;
+end
+
+try 
+    q24_firstStudy_analysis;
+catch
+    answers(1,24) = 0;
+end
+
+try 
+    q25_firstStudy_analysis;
+catch
+    answers(1,25) = 0;
+end
+
+try 
+    q26_firstStudy_analysis;
+catch
+    answers(1,26) = 0;
+end
+
+try 
+    q27_firstStudy_analysis;
+catch
+    answers(1,27) = 0;
+end
+
+try 
+    q28_firstStudy_analysis;
+catch
+    answers(1,28) = 0;
+end
+
+try 
+    q29_firstStudy_analysis;
+catch
+    answers(1,29) = 0;
+end
+
+try 
+    q30_firstStudy_analysis;
+catch
+    answers(1,30) = 0;
+end
+
+try 
+    q31_firstStudy_analysis;
+catch
+    answers(1,31) = 0;
+end
+
+try 
+    q32_firstStudy_analysis;
+catch
+    answers(1,32) = 0;
+end
+
+successOne = ['             | ', num2str(answers(1,1)) , ' | ', num2str(answers(1,2)) ,' | ', num2str(answers(1,3)), ' | ', num2str(answers(1,4)), ' | ', num2str(answers(1,5)), ' | ', num2str(answers(1,6)), ' | ', num2str(answers(1,7)), ' | ', num2str(answers(1,8)), ' | ', num2str(answers(1,9)), ' | ', num2str(answers(1,10)), '  | ', num2str(answers(1,11)), '  | ', num2str(answers(1,12)), '  | ', num2str(answers(1,13)), '  | ', num2str(answers(1,14)), '  | ', num2str(answers(1,15)), '  | ', num2str(answers(1,16)), '  | ', num2str(answers(1,17)), '  |'];
+successTwo = ['             | ', num2str(answers(1,18)), '  | ', num2str(answers(1,19)), '  | ', num2str(answers(1,20)), '  | ', num2str(answers(1,21)), '  | ', num2str(answers(1,22)), '  | ', num2str(answers(1,23)), '  | ', num2str(answers(1,24)), '  | ', num2str(answers(1,25)), '  | ', num2str(answers(1,26)), '  | ', num2str(answers(1,27)), '  | ', num2str(answers(1,28)), '  | ', num2str(answers(1,29)), '  | ', num2str(answers(1,30)), '  | ', num2str(answers(1,31)), '  | ', num2str(answers(1,32)) , '  |',];
+
+%% RESULTS DISPLAY
+disp(' ');
+disp('-----------------------------------------------------------------------------------------');
+disp('|                                      RESULTS                                          |');
+disp('-----------------------------------------------------------------------------------------');
+
+disp('              --- --- --- --- --- --- --- --- --- ---- ---- ---- ---- ---- ---- ---- ----');
+disp('objTwoColors | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 |');
+disp('              --- --- --- --- --- --- --- --- --- ---- ---- ---- ---- ---- ---- ---- ----');
+disp(successOne);
+disp('              --- --- --- --- --- --- --- --- --- ---- ---- ---- ---- ---- ---- ---- ----');
+
+disp('              ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----');
+disp('twoColorsObj | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 |');
+disp('              ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----');
+disp(successTwo);
+disp('              ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----');
+
+%% Close all plots generated.
 close all; 
